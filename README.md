a1f7c2769d6d55de24129c86ac9a62d46a56334d
Dariia Paliy <dariia.palij@gmail.com>
2023-07-07 12:44:49 +0300

    delete class Trail && added functionality && update readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # yes-or-no-app
+The yes-or-no-app is a simple application with a magical ball that provides "yes" or "no" answers to the questions you ask. It is built using HTML, SCSS for styling, JavaScript for logic, and utilizes API and the fetch function to retrieve answers from the server.
+
+<h2>Features</h2>
+Features of the project:
+
+-Ask questions: Users can input their questions into the app.
+-Get answers: The app uses a magical ball to provide "yes" or "no" answers to the questions.
+-Dynamic styling: SCSS is used to style the app, and the SCSS code is compiled into CSS for visual presentation.
+
+<h2>Technologies Used</h2>
+Technologies that were used:
+
+-HTML: The markup language used to structure the content and elements of the app.
+-SCSS/CSS: SCSS is used to write the styles for the app, which are then compiled into CSS.
+-JavaScript: The programming language used for implementing the logic and interactivity of the app.
+-API: The app interacts with an API to retrieve answers for the questions asked.
+-Fetch: The fetch function is used to make HTTP requests to the API and handle the responses.
+
+<h2>Usage</h2>
+To use the yes-or-no-app, follow these steps:
+
+    1. Clone the repository: git clone https://github.com/Darraa/yes-or-no-app.git.
+    2. Open the project directory.
+    3. Open the index.html file in your web browser.
+
+
+Once the app is loaded, you can input your questions into the input field and click the "Ask" button to get a "yes" or "no" answer from the magical ball.
--- js/script.js
+++ js/script.js
@@ -1,6 +1,93 @@
-//------------MOUSEMOVE TRAIL----------------
+const api = "https://yesno.wtf/api";
 
-import Trail from "./components/Trail.js";
+const ball = document.getElementById('ball');
+const getAnswBtn = document.getElementById("button");
+const input = document.getElementById("input");
+let ballAnswer = document.getElementById("answer");
+const error = document.getElementById('error');
 
-const trailEffect = new Trail();
-trailEffect.init();
+let isRequestInProgress = false;
+
+
+//------------CALL THE API----------------
+
+const fetchAnswer = () => {
+    setIsRequestInProgress(true)
+    setDisabledState(true);
+    ball.classList.add('shake__ball')// Add loading state
+
+    fetch(api)
+        .then((data) => data.json())
+        .then((data) => showAnswer(data.answer));
+        
+};
+
+//------------CLEANER RESPONSE----------------
+
+const cleanerResponse = () =>{
+    setTimeout(()=>{
+        ballAnswer.innerHTML = '';
+        
+        input.value = '';
+
+        setIsRequestInProgress(false);
+        setDisabledState(false);
+    }, 3000)
+};
+
+//------------ SHOW ERROR----------------
+
+const showError =()=>{
+    error.innerHTML = 'Enter the question';
+
+    setTimeout(() =>{
+        error.innerHTML= '';
+    }, 3000);
+};
+
+//------------MANAGE API REQUESTS----------------
+
+const setDisabledState = (isDisabling) =>{
+    if(isDisabling){
+        getAnswBtn.setAttribute('disabled', 'disabled');
+        input.setAttribute('disabled', 'disabled');
+    }else{
+        getAnswBtn.removeAttribute('disabled');
+        input.removeAttribute('disabled');
+    }
+};
+
+const setIsRequestInProgress = (value) =>{
+    isRequestInProgress = value;
+};
+
+const getAnswer = () =>{
+    if(isRequestInProgress) return;
+    if(!input.value) return showError();
+    fetchAnswer();
+};
+
+//------------ OUTPUT THE API'S ANSWER & ATTACH EVENT LISTENERS----------------
+
+const showAnswer = (answer) => {
+    setTimeout(() =>{        
+        ballAnswer.innerHTML = `<p>${answer}</p>`;
+        ball.classList.remove('shake__ball')// Remove loading state
+        cleanerResponse();
+    }, 1000)
+};
+
+const handleKeyEnter = (e) => {
+    if(e.keyCode === 13){
+        getAnswer();
+    }
+};
+input.addEventListener("keypress", handleKeyEnter);
+
+
+getAnswBtn.addEventListener("click", getAnswer);
+
+
+
+
+
